6401ef73dc60fdd97c5c7ad9fbe19c2b8d7e5106
onsjannet <onsjannet@gmail.com>
2020-12-07 07:37:07 +0100

    added a puppet manifest that creates a file located in /tmp

---
+++ 0x0A-configuration_managemen/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# a Puppet Manifest that creates a file located in /tmp
+file { 'holberton':
+	ensure => file,
+	path => 'tmp/holberton',
+	owner => 'www-data',
+	group => 'www-data',
+	mode => '0744',
+	content => 'I love Puppet'
+}

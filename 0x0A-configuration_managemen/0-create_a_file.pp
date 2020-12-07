df9b62a1e0a47f106a9ba0f30465ac6c318722e6
onsjannet <onsjannet@gmail.com>
2020-12-07 07:40:49 +0100

    corrected puppet lint erros

--- 0x0A-configuration_managemen/0-create_a_file.pp
+++ 0x0A-configuration_managemen/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # a Puppet Manifest that creates a file located in /tmp
 file { 'holberton':
-	ensure => file,
-	path => 'tmp/holberton',
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0744',
-	content => 'I love Puppet'
+  ensure  => file,
+  path    => 'tmp/holberton',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
 }

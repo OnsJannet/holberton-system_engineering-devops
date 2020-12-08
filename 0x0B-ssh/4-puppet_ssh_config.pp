d3777d0e50ae5db275612944cfdfaa55ae5c184f
onsjannet <onsjannet@gmail.com>
2020-12-08 20:00:06 +0100

    correcting

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,17 +1,12 @@
 # ssh_config file using puppet
-include stdlib
-
 file_line { 'Turn off passwd auth':
   ensure => 'present',
-  line   => 'PasswordAuthentication no',
   path   => '/etc/ssh/ssh_config',
-  
+  line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => 'present',
-  line   => 'IdentityFile /home/vagrant/.ssh/holberton',
   path   => '/etc/ssh/ssh_config',
-
+  line   => 'IdentityFile /home/vagrant/.ssh/holberton',
 }
-

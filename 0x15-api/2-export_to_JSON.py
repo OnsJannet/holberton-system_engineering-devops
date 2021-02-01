5fa407b3bc5d7f137f5c4f14a01826e41edee066
onsjannet <onsjannet@gmail.com>
2021-02-01 13:50:35 +0100

    correcting output

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,54 @@
 #!/usr/bin/python3
+'''
+Exports to JSON
+'''
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+
+    # api-endpoint
+    url = "https://jsonplaceholder.typicode.com/"
+
+
+    # Sending a request to get users
+    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                         .format(sys.argv[1])).json()
+
+    # Sending a request to get todos
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(sys.argv[1])).json()
+
+    '''
+    checks if a task is completed by sending
+    a get request to check the list of completed tasks
+    '''
+
+    finished_tasks = [task.get("title") for task in todos if
+                      task.get("completed") is True]
+
+    '''
+    1st line : Ex Employee Ervin Howell is done with tasks(8/20)
+    '''
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        users.get("name"), len(finished_tasks), len(todos)))
+
+    '''
+    2nd line / n lines : display the title of completed tasks:
+    '''
+
+    [print("\t {}".format(finished)) for finished in finished_tasks]
+
+    '''
+    exports to a json file
+    '''
+    with open("{}.json".format(sys.argv[1]), "w") as user_id:
+        for task in todos:
+            json.dump({sys.argv[1]: [{
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': users.get('username')
+            }]}, user_id)

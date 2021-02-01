7303c54f2981abf2093eab697a07490705ad785b
onsjannet <onsjannet@gmail.com>
2021-02-01 20:42:00 +0100

    task3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+'''
+Exports to CSV
+'''
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    # api-endpoint
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # Sending a request to get users
+    users = requests.get("https://jsonplaceholder.typicode.com/users/").json()
+
+    # Sending a request to get todos
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+
+    all_tasks = {}
+
+    '''
+    exports to a json file
+    '''
+
+    for user in users:
+        task_list = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                task_list.append(taskDict)
+        all_tasks[user.get('id')] = task_list
+    with open('todo_all_employees.json', mode='w') as user_id:
+        json.dump(all_tasks, user_id)

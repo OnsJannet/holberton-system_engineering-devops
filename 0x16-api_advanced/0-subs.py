49b336da373264e836fdde68aabc823c6fc3c802
onsjannet <onsjannet@gmail.com>
2021-02-04 17:56:03 +0100

    pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-'''
-queries the Reddit API and returns the number of subscribers
-'''
-
+"""
+Reddit API
+"""
 import json
 import requests
+"""
+queries the Reddit API and returns the number of subscribers
+"""
 
 
 def number_of_subscribers(subreddit):
-    '''
-    Reddit API
-    ''''
-
     user = {"User-Agent": "Ons_sheckler"}
     request = requests.get("https://www.reddit.com/r/{}/about.json"
                            .format(subreddit), headers=user)
-
     if request.status_code == 200:
         return request.json().get("data").get("subscribers")
     else:

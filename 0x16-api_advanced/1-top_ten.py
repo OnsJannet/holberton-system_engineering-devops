f1ff45353c3c346a02080bc9b3cce1d1ec34499c
onsjannet <onsjannet@gmail.com>
2021-02-04 18:44:26 +0100

    task 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Reddit API
+"""
+import json
+import requests
+"""
+queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
+
+
+def top_ten(subreddit):
+    user = {"User-Agent": "Ons_sheckler"}
+    request = requests.get("https://www.reddit.com/r/{}/hot/.json?limit=10"
+                           .format(subreddit), headers=user)
+    if request.status_code == 200:
+        for posts in request.json().get("data").get("children"):
+            print(posts.get("data").get("title"))
+    else:
+        print("None")

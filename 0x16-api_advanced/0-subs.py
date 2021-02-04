1843ed3e04528766c56c6168317b3f0d09276444
onsjannet <onsjannet@gmail.com>
2021-02-04 14:06:19 +0100

    added a script that sends a request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+import requests
+import json
+
+'''
+queries the Reddit API and returns the number of subscribers
+'''
+
+
+def number_of_subscribers(subreddit):
+
+    user = {"User-Agent": "Ons_sheckler"}
+    request = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers=user)
+
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
+    else:
+        return 0
+
+# 1 enter the url on my browser 'https://www.reddit.com/r/programming/.json'
+# 2 press f12 / inspect element and refresh page
+# 3 go to network - all
+# 4 copy as cURL
+# 5 paste in https://curl.trillworks.com/ a curl converter to python request
+# 6 look for the header ' User-Agent'
+# 7 the json request have 2 keys Data and Kind
+# 8 look for 'subscribers' in the 'Data' list

39ab9a98dc670ea4cb8afbdf450470e77f86d014
onsjannet <onsjannet@gmail.com>
2021-02-04 17:56:49 +0100

    pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,3 +17,14 @@ def number_of_subscribers(subreddit):
         return request.json().get("data").get("subscribers")
     else:
         return 0
+
+# 1 enter the url on my browser 'https://www.reddit.com/r/programming/.json'
+# 2 press f12 / inspect element and refresh page
+# 3 go to network - all
+# 4 copy as cURL
+# 5 paste in https://curl.trillworks.com/ a curl converter to python request
+# 6 look for the header ' User-Agent'
+# 7 the json request have 2 keys Data and Kind
+# 8 look for 'subscribers' in the 'Data' list
+
+#for more information: https://www.youtube.com/watch?v=Mw-dsY8UKVs

19ac64310254c87dc0a45d55e8d1dc59c9bd3229
onsjannet <onsjannet@gmail.com>
2021-02-17 19:35:30 +0100

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,10 @@
-
-  exec { 'fix typo' :
-    ensure   => present,
-    } ->
-  file_line { 'replace':
-    path    => /var/www/html/wp-settings.php,
-    replace => true,
-    line    => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-    match   => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );"
-  }
+  file { '/var/www/html/wp-settings.php':
+  ensure  => present,
+}->
+file_line { 'replace':
+  path    => '/var/www/html/wp-settings.php',
+  replace => true,
+  line    => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
+  match   => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp",
+}
 
